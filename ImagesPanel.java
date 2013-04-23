397a8af86cf01ae6cd506e92bf6d1f8fc600a84f
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2013-04-22 21:28:11 -0500

    Reduce search space from 280x280 to 10x10

--- ImagesPanel.java
+++ ImagesPanel.java
@@ -16,15 +16,37 @@ public class ImagesPanel extends JPanel {
 	
 	public void paint(Graphics g) {
 		BufferedImage image = Shared.recognitionWin.getImage();
+		int[] rCoords = Shared.recognitionWin.getRectangleCoords();
+		for (int i = 0; i < rCoords.length; i++) rCoords[i] += 50;
+		boolean[][] bits = Shared.recognitionWin.getImageBits();
 		if (image != null)
 			g.drawImage(image, 50, 50, this);
+		g.setColor(Color.BLACK);
 		g.fillRect(379, 49, 281, 281);
+		g.setColor(Color.GREEN);
+		g.drawLine(379, 49, 380, 49);
 		g.setColor(Color.WHITE);
 		for (int i = 408; i < 660; i+= 28) {
 			g.drawLine(i, 50, i, 330);
 			g.drawLine(380, i - 330, 660, i - 330);
 		}
-		
+		if (bits != null)
+			for (int i = 0; i < 10; i++)
+				for (int j = 0; j < 10; j++)
+					if (bits[i][j])
+						g.fillRect(380 + 28*i, 50 + 28*j, 28, 28);
+		if (rCoords != null) {
+			/*int w = (rCoords[2] - rCoords[0])/2;
+			int h = (rCoords[3] - rCoords[1])/2; 
+			g.setColor(Color.GRAY);
+			for (int i = 1; i < 10; i++) {
+				g.drawLine(rCoords[0] + i*w/5, rCoords[1], rCoords[0] + i*w/5, rCoords[3]);
+				g.drawLine(rCoords[0], rCoords[1] + i*h/5, rCoords[2], rCoords[1] + i*h/5);
+			}*/
+			g.setColor(Color.RED);
+			g.drawRect(rCoords[0] - 1, rCoords[1] - 1,
+				rCoords[2] - rCoords[0] + 1, rCoords[3] - rCoords[1] + 2);	
+		}
 	}
 	
 }
--- RecognitionWin.java
+++ RecognitionWin.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -8,11 +9,13 @@ import javax.swing.JFrame;
 
 @SuppressWarnings("serial")
 public class RecognitionWin extends JFrame {
-	
+
 	private ImagesPanel imagesPanel;
 	private JButton btnReset;
 	private BufferedImage image;
-	
+	private int[] rectCoords;
+	private boolean[][] bits;
+
 	public RecognitionWin() {
 		setTitle("Handwritten Digit Recognition");
 		setSize(710, 480);
@@ -20,7 +23,7 @@ public class RecognitionWin extends JFrame {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setResizable(false);
 		setLayout(null);
-		
+
 		ImagesPanel imagesPanel = new ImagesPanel();
 		btnReset = new JButton("Reset");
 		btnReset.setBounds(565, 400, 100, 25);
@@ -29,29 +32,64 @@ public class RecognitionWin extends JFrame {
 		getContentPane().add(btnReset);
 		getContentPane().add(imagesPanel);
 	}
-	
+
 	public void loadImage() {
 		boolean[][] data = Shared.drawPanel.getData();
 		image = new BufferedImage(280, 280, BufferedImage.TYPE_BYTE_BINARY);
+		rectCoords = new int[] {280, 280, 0, 0}; // xmin - ymin - xmax - ymax
 		for (int i = 0; i < data.length; i++)
 			for (int j = 0; j < data[i].length; j++) {
 				int color = (data[i][j])? -1 : -16777216;
+				if (data[i][j]) {
+					if (i < rectCoords[0]) rectCoords[0] = i;
+					if (j < rectCoords[1]) rectCoords[1] = j;
+					if (i > rectCoords[2]) rectCoords[2] = i;
+					if (j > rectCoords[3]) rectCoords[3] = j;
+				}
 				image.setRGB(i, j, color);
 			}
+		bits = new boolean[10][10];
+		int w = rectCoords[2] - rectCoords[0];
+		int h = rectCoords[3] - rectCoords[1];
+		// CREATE BITS ARRAY
+		int x = -1, y = -1, dx = w/10, dy = h/10;
+		for (int i = rectCoords[0]; i <= rectCoords[2]; i++) {
+			if ((i - rectCoords[0]) % dx == 0) x++;
+			if (x == 10) x = 9;
+			y = -1;
+			for (int j = rectCoords[1]; j <= rectCoords[3]; j++) {
+				if ((j - rectCoords[1]) % dy == 0) y++;
+				if (y == 10) y = 9;
+				if (bits[x][y]) continue;
+				System.out.println(x + " " + y);
+				bits[x][y] = data[i][j] || bits[x][y];
+			}
+		}
+		for (boolean[] a : bits) {
+			System.out.println(Arrays.toString(a));
+		}
 		if (imagesPanel != null) imagesPanel.repaint();
 	}
-	
+
+	public int[] getRectangleCoords() {
+		return rectCoords;
+	}
+
+	public boolean[][] getImageBits() {
+		return bits;
+	}
+
 	public BufferedImage getImage() {
 		return image;
 	}
-	
+
 	private class ResetButtonListener implements ActionListener {
 
 		public void actionPerformed(ActionEvent e) {
 			dispose();
 			Shared.drawWin = new DrawWin();
 		}
-		
+
 	}
-	
+
 }
