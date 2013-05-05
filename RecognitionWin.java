b03dab65ac86c916ddf1bca4ba9c40fb085751cc
Miguel Flores Ruiz de Eguino <miguel.frde@gmail.com>
2013-05-05 14:48:15 -0500

    Add ImageUtils class

--- RecognitionWin.java
+++ RecognitionWin.java
@@ -3,10 +3,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+
+import utils.ImageUtils;
 
 @SuppressWarnings("serial")
 public class RecognitionWin extends JFrame {
@@ -34,7 +35,7 @@ public class RecognitionWin extends JFrame {
 		btnReset.setBounds(565, 400, 100, 25);
 		btnReset.setFocusPainted(false);
 		btnReset.addActionListener(new ResetButtonListener());
-		lblDigit = new JLabel(new ImageIcon("res/loader.gif"));
+		lblDigit = new JLabel("");
 		lblDigit.setFont(new Font("Verdana", Font.PLAIN, 100));
 		lblDigit.setBounds(280, 340, 150, 100);
 		getContentPane().add(btnReset);
@@ -47,36 +48,9 @@ public class RecognitionWin extends JFrame {
 	 */
 	public void loadImage() {
 		boolean[][] data = Shared.drawPanel.getData();
-		image = new BufferedImage(280, 280, BufferedImage.TYPE_BYTE_BINARY);
-		
-		// Obtain the image and the rectangle that contains it
-		rectCoords = new int[] {280, 280, 0, 0}; // { xmin, ymin, xmax, ymax }
-		for (int i = 0; i < data.length; i++)
-			for (int j = 0; j < data[i].length; j++) {
-				int color = (data[i][j])? -1 : -16777216;
-				if (data[i][j]) {
-					if (i < rectCoords[0]) rectCoords[0] = i;
-					if (j < rectCoords[1]) rectCoords[1] = j;
-					if (i > rectCoords[2]) rectCoords[2] = i;
-					if (j > rectCoords[3]) rectCoords[3] = j;
-				}
-				image.setRGB(i, j, color);
-			}
-		
-		// Obtain the bits for the reduced 10x10 image
-		bits = new boolean[10][10];
-		int w = rectCoords[2] - rectCoords[0];
-		int h = rectCoords[3] - rectCoords[1];
-		int x = -1, y = -1, dx = w/10, dy = h/10;
-		for (int i = rectCoords[0]; i <= rectCoords[2]; i++) {
-			if (x != 9 && (i - rectCoords[0]) % dx == 0) x++;
-			y = -1;
-			for (int j = rectCoords[1]; j <= rectCoords[3]; j++) {
-				if (y != 9 && (j - rectCoords[1]) % dy == 0) y++;
-				if (bits[x][y]) continue;
-				bits[x][y] = data[i][j] || bits[x][y];
-			}
-		}
+		image = ImageUtils.getImage(data);
+		rectCoords = ImageUtils.getRectangle(data);
+		bits = ImageUtils.getBits(rectCoords, data);
 	}
 
 	/**
---
+++ utils/ImageUtils.java
@@ -0,0 +1,49 @@
+package utils;
+
+import java.awt.image.BufferedImage;
+
+public class ImageUtils {
+	
+	public static BufferedImage getImage(boolean[][] data) {
+		BufferedImage image = new BufferedImage(280, 280, BufferedImage.TYPE_BYTE_BINARY);
+		for (int i = 0; i < data.length; i++)
+			for (int j = 0; j < data[i].length; j++) {
+				int color = (data[i][j])? -1 : -16777216;
+				image.setRGB(i, j, color);
+			}
+		return image;
+	}
+	
+	public static int[] getRectangle(boolean[][] data) {
+		int[] rectCoords = new int[] {280, 280, 0, 0}; // { xmin, ymin, xmax, ymax }
+		for (int i = 0; i < data.length; i++)
+			for (int j = 0; j < data[i].length; j++)
+				if (data[i][j]) {
+					if (i < rectCoords[0]) rectCoords[0] = i;
+					if (j < rectCoords[1]) rectCoords[1] = j;
+					if (i > rectCoords[2]) rectCoords[2] = i;
+					if (j > rectCoords[3]) rectCoords[3] = j;
+				}
+		return rectCoords;
+	}
+	
+	public static boolean[][] getBits(int[] rectCoords, boolean[][] data) {
+		boolean bits[][] = new boolean[10][10];
+		int w = rectCoords[2] - rectCoords[0];
+		int h = rectCoords[3] - rectCoords[1];
+		int x = -1, y = -1, dx = w/10, dy = h/10;
+		if (dx == 0) dx = 1;
+		if (dy == 0) dy = 1;
+		for (int i = rectCoords[0]; i <= rectCoords[2]; i++) {
+			if (x != 9 && (i - rectCoords[0]) % dx == 0) x++;
+			y = -1;
+			for (int j = rectCoords[1]; j <= rectCoords[3]; j++) {
+				if (y != 9 && (j - rectCoords[1]) % dy == 0) y++;
+				if (bits[x][y]) continue;
+				bits[x][y] = data[i][j] || bits[x][y];
+			}
+		}
+		return bits;
+	}
+	
+}
